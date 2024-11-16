2485145f5badfcf893c43c252c028110552d7607
Gowtham P <71524339+gowthamp10@users.noreply.github.com>
2024-11-16 23:57:18 +0530

    Update helloworld.sh

--- TryHackMe/Rooms/BashScripting/Scripts/helloworld.sh
+++ TryHackMe/Rooms/BashScripting/Scripts/helloworld.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-echo "Hello World!"
-whoami
-id
+echo "Hello World!"                          # Prints Hello World! on the screen
+whoami                                       # Prints who the user is
+id                                           # Provides id details as part of user identity such as uid, gid etc
