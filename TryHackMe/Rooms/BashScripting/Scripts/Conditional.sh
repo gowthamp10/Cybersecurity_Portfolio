034f47d97fcb027bede28d0aa45fc1aed6218d1d
Gowtham P <71524339+gowthamp10@users.noreply.github.com>
2024-11-22 09:09:29 +0530

    Create Conditional.sh

---
+++ TryHackMe/Rooms/BashScripting/Scripts/Conditional.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+count=10
+if [ $count -eq 10 ]
+then
+  echo "True"
+else
+  echo "False"
+fi
+
+# comparision can be done using these '-eq', '-gt', '-lt', '-ne', '-ge'. These are few examples
